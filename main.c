daa2a5d3caf75c1be35901f41d06130876274881
moeweli89 <moe.moe89@icloud.com>
2025-05-26 01:01:29 +0200

    uppgift2

--- main.c
+++ main.c
@@ -2,7 +2,14 @@
 
 
 // This is where you add the two functions
+float fahrenheit_to_celsius(float fahrenheit) {
+    return (fahrenheit - 32) * 5.0 / 9.0;
+}
 
+
+float celsius_to_fahrenheit(float celsius) {
+    return (celsius * 9.0 / 5.0) + 32;
+}
 int main() {
     float temp_f = 100.0;
     float temp_c = 37.0;
